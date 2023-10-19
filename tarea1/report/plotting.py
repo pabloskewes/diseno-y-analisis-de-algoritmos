9fbef3f0f0aa152f69098d7ab4df330c217da044
Pablo <pablo.skewes@ug.uchile.cl>
2023-10-19 04:43:39 -0300

    added first notebook version

---
+++ tarea1/report/plotting.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def build_lines_fig(
+    means: pd.DataFrame,
+    title: str = None,
+    yaxis_title: str = None,
+    scale: str = "linear",
+) -> go.Figure:
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(
+            x=means.index,
+            y=means["nearestx"],
+            name="Nearest X",
+            mode="lines",
+            line=dict(color="rgb(31, 119, 180)"),
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=means.index,
+            y=means["hilbert"],
+            name="Hilbert Curve",
+            mode="lines",
+            line=dict(color="rgb(255, 127, 14)"),
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=means.index,
+            y=means["sort_tile_recursive"],
+            name="Sort Tile Recursive",
+            mode="lines",
+            line=dict(color="rgb(44, 160, 44)"),
+        )
+    )
+
+    fig.update_layout(
+        title=title,
+        xaxis_title="Number of points",
+        yaxis_title=yaxis_title,
+        yaxis_type=scale,
+        legend_title="Algorithm",
+    )
+    fig.update_xaxes(tickangle=0)
+
+    return fig
+
+
+def build_comparison_boxplot(
+    means: pd.DataFrame,
+    title: str = None,
+    yaxis_title: str = None,
+    scale: str = "linear",
+) -> go.Figure:
+    """
+    Uses plotly to plot the execution time for each algorithm.
+    Plots all 3 box plots in the same figure in log scale.
+    """
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Box(
+            y=means["nearestx"],
+            name="Nearest X",
+            boxmean=True,
+            marker_color="darkorange",
+            boxpoints="all",
+            jitter=0.3,
+            pointpos=-1.8,
+        )
+    )
+
+    fig.add_trace(
+        go.Box(
+            y=means["hilbert"],
+            name="Hilbert Curve",
+            boxmean=True,
+            marker_color="darkblue",
+            boxpoints="all",
+            jitter=0.3,
+            pointpos=-1.8,
+        )
+    )
+
+    fig.add_trace(
+        go.Box(
+            y=means["sort_tile_recursive"],
+            name="Sort Tile Recursive",
+            boxmean=True,
+            marker_color="darkgreen",
+            boxpoints="all",
+            jitter=0.3,
+            pointpos=-1.8,
+        )
+    )
+
+    fig.update_layout(
+        title=title,
+        xaxis_title="Number of points",
+        yaxis_title=yaxis_title,
+        yaxis_type=scale,
+    )
+
+    return fig
---
+++ tarea1/report/processing.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+def inject_header(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Injects a header into the dataframe.
+    Transforms the current header into the first row.
+    """
+    df = df.copy()
+    header = [f"q{i}" for i in range(1, 101)]
+
+    first_row = df.iloc[0].copy()
+    df.columns = header
+    df.index = df.index + 1
+    df.iloc[0] = first_row
+
+    return df
+
+
+def inject_n_index(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Injects a index with the input size into the dataframe.
+    """
+
+    df = df.copy()
+    # x
+    powers_of_2 = [
+        "2¹⁰",
+        "2¹¹",
+        "2¹²",
+        "2¹³",
+        "2¹⁴",
+        "2¹⁵",
+        "2¹⁶",
+        "2¹⁷",
+        "2¹⁸",
+        "2¹⁹",
+        "2²⁰",
+        "2²¹",
+        "2²²",
+        "2²³",
+        "2²⁴",
+        "2²⁵",
+    ]
+
+    # df.index = [f"2^{i}" for i in range(10, 25)]
+    df.index = powers_of_2
+    return df
+
+
+def load_data(path: Path) -> pd.DataFrame:
+    """
+    Loads data from a csv file and injects a header and
+    a column with the input size.
+    """
+    df = pd.read_csv(path, usecols=range(100), header=None)
+    df = inject_header(df)
+    df = inject_n_index(df)
+    return df
