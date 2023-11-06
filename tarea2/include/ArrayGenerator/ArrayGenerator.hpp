7b51743838f8e30261262d10c0e7c2841d6cd816
Pablo <pablo.skewes@ug.uchile.cl>
2023-11-05 23:44:30 -0300

    simplified array generator

--- tarea2/include/ArrayGenerator/ArrayGenerator.hpp
+++ tarea2/include/ArrayGenerator/ArrayGenerator.hpp
@@ -1,16 +1,12 @@
 #ifndef ARRAY_GENERATOR_HPP
 #define ARRAY_GENERATOR_HPP
 
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-vector<unsigned long long> generateRandomArray(int arraySize,
-                                               unsigned long long minNumber,
-                                               unsigned long long maxNumber);
-void saveArrayToFile(string fileName, vector<unsigned long long> numbers);
-vector<unsigned long long> loadArrayFromFile(string fileName);
-void generateArrays();
+vector<unsigned long long>
+generateRandomArray(int arraySize, unsigned long long universePower);
 
-#endif //ARRAY_GENERATOR_HPP
+#endif // ARRAY_GENERATOR_HPP
--- tarea2/src/main.cpp
+++ tarea2/src/main.cpp
@@ -1,12 +1,18 @@
-#include <iostream>
 #include <ArrayGenerator/ArrayGenerator.hpp>
+#include <iostream>
 
 using namespace std;
 
 int main() {
     cout << "Hello world!" << endl;
 
-    generateArrays();
-    
+    int arraySize = 100;
+    int universePower = 64;
+    vector<unsigned long long> numbers = generateRandomArray(arraySize, universePower); 
+
+    for (int i = 0; i < arraySize; i++) {
+        cout << numbers[i] << endl;
+    }
+
     return 0;
 }
