79153e933d66796a646f9e06df217fd416b00feb
Pablo <pablo.skewes@ug.uchile.cl>
2023-11-08 03:21:20 -0300

    implemented experiments functions

--- tarea2/src/main.cpp
+++ tarea2/src/main.cpp
@@ -1,31 +1,140 @@
-#include <Array/ArrayGenerator.hpp>
 #include <Array/ArrayChecker.hpp>
+#include <Array/ArrayGenerator.hpp>
+#include <SortAlgorithm/MergeSort.hpp>
 #include <SortAlgorithm/QuickSort.hpp>
 #include <SortAlgorithm/RadixSort.hpp>
-#include <iostream>
+#include <chrono>
 #include <cmath>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    cout << "Hello world!" << endl;
+/**
+ * @brief Writes the result of an experiment to a file
+ * @param baseFileName The base file name
+ * @param universePower The universe power
+ * @param numExperiment The experiment number
+ * @param duration The duration of the experiment
+ */
+void writeMergeSortExperimentResult(string baseFileName, int universePower,
+                                    int numExperiment, int duration) {
+    string fileName = baseFileName + "u" + to_string(universePower) + "_exp" +
+                      to_string(numExperiment) + ".csv";
+    ofstream file(fileName, ios::app);
+    file << universePower << "," << numExperiment << "," << duration << endl;
+    file.close();
+}
 
-    // int arraySize = 100;
-    int arraySize = pow(10, 7);
-    int universePower = 64;
-    vector<unsigned long long> numbers = generateRandomArray(arraySize, universePower); 
+/**
+ * @brief Writes the result of an experiment to a file
+ * @param baseFileName The base file name
+ * @param universePower The universe power
+ * @param k The number of bits to extract
+ * @param numExperiment The experiment number
+ * @param duration The duration of the experiment
+ */
+void writeRadixSortExperimentResult(string baseFileName, int universePower,
+                                    int k, int numExperiment, int duration) {
+    string fileName = baseFileName + "u" + to_string(universePower) + "_k" +
+                      to_string(k) + "_exp" + to_string(numExperiment) + ".csv";
+    ofstream file(fileName, ios::app);
+    file << universePower << "," << k << "," << numExperiment << "," << duration
+         << endl;
+    file.close();
+}
 
-    cout << "Array size: " << numbers.size() << endl;
-    cout << "Universe power: " << universePower << endl;
-
+/**
+ * @brief Runs an experiment with merge sort
+ * @param outputPath The output path
+ * @param numbers The numbers to sort
+ * @param universePower The universe power
+ * @param numExperiment The experiment number
+ */
+void runMergeSortExperiment(string outputPath,
+                            vector<unsigned long long> &numbers,
+                            int universePower, int numExperiment) {
     vector<unsigned long long> numbersCopy = numbers;
 
-    // quickSort(numbers);
-    int k = 15;
-    radixSort(numbers, k);
+    auto start = chrono::high_resolution_clock::now();
+    mergeSort(numbersCopy);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
 
-    cout << "Is sorted: " << isSorted(numbers) << endl;
-    cout << "Has same elements: " << hasSameElements(numbers, numbersCopy) << endl;
+    if (isSorted(numbersCopy)) {
+        writeMergeSortExperimentResult(outputPath, universePower, numExperiment,
+                                       duration.count());
+    } else {
+        cout << "Merge sort failed" << endl;
+        cout << "Universe power: " << universePower << endl;
+    }
+
+    numbersCopy.clear();
+
+    return;
+}
+
+/**
+ * @brief Runs an experiment with radix sort
+ * @param outputPath The output path
+ * @param numbers The numbers to sort
+ * @param universePower The universe power
+ * @param k The number of bits to extract
+ * @param numExperiment The experiment number
+ */
+void runRadixSortExperiment(string outputPath,
+                            vector<unsigned long long> &numbers,
+                            int universePower, int k, int numExperiment) {
+    vector<unsigned long long> numbersCopy = numbers;
+
+    auto start = chrono::high_resolution_clock::now();
+    radixSort(numbersCopy, k);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+
+    if (isSorted(numbersCopy)) {
+        writeRadixSortExperimentResult(outputPath, universePower, k,
+                                       numExperiment, duration.count());
+    } else {
+        cout << "Radix sort failed" << endl;
+        cout << "Universe power: " << universePower << endl;
+        cout << "k: " << k << endl;
+    }
+
+    numbersCopy.clear();
+
+    return;
+}
+
+int main() {
+    cout << "Starting experiments" << endl;
+
+    int arraySize = pow(10, 7);
+    int maxK = 35;
+    string mergeSortOutputPath = "data/mergeSortResults/";
+    string radixSortOutputPath = "data/radixSortResults/";
+
+    for (int universePower = 1; universePower <= 64; universePower++) {
+        cout << "Universe power: " << universePower << endl;
+        for (int numExperiment = 1; numExperiment <= 100; numExperiment++) {
+            cout << "Experiment number: " << numExperiment << endl;
+            vector<unsigned long long> numbers =
+                generateRandomArray(arraySize, universePower);
+
+            cout << "Running merge sort" << endl;
+            runMergeSortExperiment(mergeSortOutputPath, numbers, universePower,
+                                   numExperiment);
+
+            cout << "Running radix sort" << endl;
+            for (int k = 1; k <= min(maxK, universePower); k++) {
+                cout << "k: " << k << endl;
+                runRadixSortExperiment(radixSortOutputPath, numbers,
+                                       universePower, k, numExperiment);
+            }
+
+            numbers.clear();
+        }
+    }
 
     return 0;
 }
