0d51bdb918dce49431e8e54c9f444b02f39a0203
Pablo <pablo.skewes@ug.uchile.cl>
2023-11-26 17:54:52 -0300

    implemented grid structs and functions
    structs: point and grid
    - generateGrid
    - loadGrid
    - writeGrid
    
    tested on main and working well

--- tarea3/src/main.cpp
+++ tarea3/src/main.cpp
@@ -1,8 +1,25 @@
+#include "Grid/Grid.hpp"
 #include <iostream>
 
 using namespace std;
 
+string filename = "data/grids/grid_100.bin";
+
 int main() {
     cout << "Hello, World!" << endl;
-    return 0;
+
+    Grid generatedGrid = generateGrid(10000);
+    writeGrid(generatedGrid, filename);
+    Grid loadedGrid = loadGrid(filename);
+
+    bool equal = true;
+    for (int i = 0; i < generatedGrid.points.size(); i++) {
+        if (generatedGrid.points[i].x != loadedGrid.points[i].x ||
+            generatedGrid.points[i].y != loadedGrid.points[i].y) {
+            equal = false;
+            break;
+        }
+    }
+
+    cout << "Equal: " << equal << endl;
 }
