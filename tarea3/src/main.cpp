9a5209aa8fa17866520a7ee4152aec45cb4207eb
sebabrzovic <sebastian.brzovic@ug.uchile.cl>
2023-11-26 19:11:47 -0300

    Revert "Merge branch 'main' of github.com:pabloskewes/diseno-y-analisis-de-algoritmos"
    This reverts commit 336fab701a34e3c8142f94716ecd975ef58be098, reversing
    changes made to 3f7fe237f002a582a9abd773a42ffeb5d5891a27.

--- tarea3/src/main.cpp
+++ tarea3/src/main.cpp
@@ -1,21 +1,8 @@
-#include "Grid/Grid.hpp"
-#include "Grid/bulkGeneration.hpp"
 #include <iostream>
 
 using namespace std;
 
-string gridsBaseDir = "data/grids/";
-
 int main() {
     cout << "Hello, World!" << endl;
-
-    // Generate grids
-    // bulkGenerateGrids(gridsBaseDir);
-
-    // Load grid
-    for (int i = 5; i <= 50; i += 5) {
-        string filename = gridsBaseDir + to_string(i) + ".bin";
-        Grid grid = loadGrid(filename);
-        printGrid(grid);
-    }
+    return 0;
 }
